9bc52a073763ed5aa1a87de44d80313520c9b97b
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-02-19 11:12:39 -0300

    lesson: finish game

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rocket, Paper and Scissor
+
+## Descrição
+
+Command Line Application (CLI) usando o dart que simula um jogo de pedra, papel e tesoura com a máquina.
--- main.dart
+++ main.dart
@@ -1,12 +1,41 @@
 import 'dart:io';
+import 'dart:math';
+
+enum Move { rock, paper, scissors }
 
 void main() {
+  final rng = Random();
+
   while (true) {
     stdout.write('Rock, paper or scissor? (r/p/s) ');
     final input = stdin.readLineSync();
 
     if (input == 'r' || input == 'p' || input == 's') {
-      print('Selected $input');
+      Move playerMove;
+
+      if (input == 'r') {
+        playerMove = Move.rock;
+      } else if (input == 'p') {
+        playerMove = Move.paper;
+      } else {
+        playerMove = Move.scissors;
+      }
+
+      final random = rng.nextInt(3);
+      final aiMove = Move.values[random];
+
+      print('You played ${playerMove.name}');
+      print('AI played ${aiMove.name}');
+
+      if (playerMove == aiMove) {
+        print("It's a draw");
+      } else if ((playerMove == Move.rock && aiMove == Move.scissors) ||
+          (playerMove == Move.paper && aiMove == Move.rock) ||
+          (playerMove == Move.scissors && aiMove == Move.paper)) {
+        print('You win');
+      } else {
+        print('You lose');
+      }
     } else if (input == 'q') {
       break;
     } else {
